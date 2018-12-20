47f26a23cc5d267ae7d6b57bcdf10ddb3c35d7b8
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2018-12-20 19:59:56 +0530

    Encoded categorical data

--- ann.py
+++ ann.py
@@ -1,10 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
-from sklearn.model_selection import train_test_split
+
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
