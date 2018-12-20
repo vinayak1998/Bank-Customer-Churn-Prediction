6d2a96adb2b8a228a4f3c0e6cbd7c73edf1f5999
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2018-12-20 19:22:52 +0530

    Read data

---
+++ ann.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
