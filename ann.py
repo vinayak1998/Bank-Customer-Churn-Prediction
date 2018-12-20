9309cd73d86e4ec32ec78d493f491a496ba6fe31
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2018-12-20 21:12:42 +0530

    Geog. one-hot encoded

--- ann.py
+++ ann.py
@@ -16,5 +16,8 @@ X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:] #for dummy variable set
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
