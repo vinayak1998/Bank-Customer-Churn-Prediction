0e18297f0ccd018f09a273f33f5434c8e0b06d19
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2018-12-20 22:44:50 +0530

    Feat. scaling

--- ann.py
+++ ann.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 dataset = pd.read_csv('Churn_Modelling.csv')
@@ -20,4 +21,9 @@ onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:] #for dummy variable set
 
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
