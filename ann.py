2df12c5ab8956ea0fad4ee9cdd7451c34b10e085
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2018-12-28 17:40:25 +0530

    output layer

--- ann.py
+++ ann.py
@@ -36,3 +36,6 @@ classifier = Sequential()
 
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
